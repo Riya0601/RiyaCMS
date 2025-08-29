642eaf2c51d5f0587cad583023d5d7bbe240d0fc
Riya0601 <83570041+Riya0601@users.noreply.github.com>
2025-08-29 12:25:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+console.log("JS loaded ✅");
+
 let toggle = true;
 
-function toggleImage() {
+document.addEventListener("DOMContentLoaded", () => {
   const img = document.getElementById("moonImg");
-  if (toggle) {
-    img.src = "https://upload.wikimedia.org/wikipedia/commons/9/99/Partial_Moon.jpg";
-  } else {
-    img.src = "https://upload.wikimedia.org/wikipedia/commons/e/e1/FullMoon2010.jpg";
-  }
-  toggle = !toggle;
-}
+
+  img.addEventListener("click", () => {
+    if (toggle) {
+      img.src = "https://upload.wikimedia.org/wikipedia/commons/9/99/Partial_Moon.jpg";
+    } else {
+      img.src = "https://upload.wikimedia.org/wikipedia/commons/e/e1/FullMoon2010.jpg";
+    }
+    toggle = !toggle;
+    console.log("Image changed:", img.src);
+  });
+});
